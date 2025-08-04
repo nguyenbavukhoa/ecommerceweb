fa545d987b16aa95ae6dc7ec1f13140a90f27f06
Minh Hao <dangminhhao365@gmail.com>
2025-08-04 22:51:22 +0700

    update UI orderpage

---
+++ FrontEnd/src/pages/OrderPage/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+export const WrapperStyled = styled.div`
+  padding: 20px;
+  background-color: #ffffff;
+`;
+
+export const TitleStyled = styled.h2`
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+export const OrderSearchStyled = styled.div`
+  margin-bottom: 20px;
+`;
