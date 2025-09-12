ead1bb4ba98564330cda1e9b15453187aeab0df4
trungkien <145746252+allpedog@users.noreply.github.com>
2025-09-12 23:56:12 +0700

    payment

--- Backend/src/main/java/com/e_commerce/dto/payment/PaymentDTO/PaymentDTO.java
+++ Backend/src/main/java/com/e_commerce/dto/payment/PaymentDTO/PaymentDTO.java
@@ -1,4 +1,10 @@
 package com.e_commerce.dto.payment.PaymentDTO;
 
+import lombok.Data;
+
+@Data
 public class PaymentDTO {
+    public String code;
+    public String message;
+    public String paymentUrl;
 }
