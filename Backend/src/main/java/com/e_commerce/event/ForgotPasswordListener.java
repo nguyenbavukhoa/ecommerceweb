63d0b346d16c2fe97ad49eb43d86ec2e2142658d
trungkien <145746252+allpedog@users.noreply.github.com>
2025-09-28 18:04:01 +0700

    refactor(async): move email sending to async execution

--- Backend/src/main/java/com/e_commerce/event/ForgotPasswordListener.java
+++ Backend/src/main/java/com/e_commerce/event/ForgotPasswordListener.java
@@ -4,6 +4,7 @@ import com.e_commerce.service.email.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,6 +16,7 @@ public class ForgotPasswordListener implements ApplicationListener<ForgotPasswor
         this.emailService = emailService;
         this.otpExpirationMinutes = otpExpirationMinutes;
     }
+
 
     @Override
     public void onApplicationEvent(ForgotPasswordEvent event) {
