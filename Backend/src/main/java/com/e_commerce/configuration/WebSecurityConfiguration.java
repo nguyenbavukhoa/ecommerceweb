b0eb7c14fa8bbf74487e34105d26cf183ec64d7d
Minh Hao <dangminhhao365@gmail.com>
2025-11-25 18:06:11 +0700

    fix

--- Backend/src/main/java/com/e_commerce/configuration/WebSecurityConfiguration.java
+++ Backend/src/main/java/com/e_commerce/configuration/WebSecurityConfiguration.java
@@ -48,6 +48,7 @@ public class WebSecurityConfiguration {
                         .requestMatchers(HttpMethod.POST,"auth/reset-password").permitAll()
                         .requestMatchers(HttpMethod.POST,"auth/verify-otp").permitAll()
                         .requestMatchers(HttpMethod.GET,"products/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
 
                         .anyRequest()
                         .authenticated())
